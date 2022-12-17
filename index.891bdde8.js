ea43ef204cae8e9aa5b32ca74abef69c3da087c3
Jacek-85 <111233513+Jacek-85@users.noreply.github.com>
2022-12-17 23:06:35 +0000

    Deploying to gh-pages from @ Jacek-85/Ice_Muuu@b75f34c141dd65ae7f6339cefb298590a62791d3 🚀

---
+++ index.891bdde8.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".header__mobile-menu"),n=document.querySelector(".header__exit-btn");document.querySelector(".header__mobile-btn").addEventListener("click",(function(n){e.classList.remove("d-none")})),n.addEventListener("click",(function(n){e.classList.add("d-none")}))}();
+//# sourceMappingURL=index.891bdde8.js.map
